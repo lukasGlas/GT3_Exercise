23d12d0414fe563a1de585937ce12dbcf56e7d9e
MaxArchxd <82872188+MaxArchxd@users.noreply.github.com>
2024-11-25 16:34:19 +0100

    Async Scene Loading Aufgabe

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    /*  Obwohl Awaitable in dieser Unity-Version noch nicht verfügbar ist,
+     *  kann Code asynchron ausgeführt werden. Ersetze LoadScene() mit
+     *  LoadSceneAsync(), um das Laden der Szene auf einen Hintergrund-Thread
+     *  zu verlagern.
+     *  
+     *  Gestalte vielleicht noch einen hübschen Ladebalken, der während dem
+     *  Laden der Szene angezeigt und AsyncOperation.progress aktualisiert wird.
+     */
+    public void startLoading(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+        //StartCoroutine(LoadYourAsyncScene(sceneName));
+    }
+
+    //Musterlösung, löschen!
+    IEnumerator LoadYourAsyncScene(string sceneName)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            print("Loading...");
+            yield return null;
+        }
+    }
+}
